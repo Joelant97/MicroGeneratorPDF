0d013ac1049195fa824249c17e56614b3db0209d
joelant97 <joelant97@gmail.com>
2024-01-14 12:20:53 -0400

    Created Microservices Skeleton+Contracts&Services

---
+++ TemplateManager.Application/Services/v1/ITemplateConfigService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateManager.Application.Contracts.v1.Templates.Response;
+
+namespace TemplateManager.Application.Services.v1
+{
+    public interface ITemplateConfigService
+    {
+        public Task<GetTemplatesResponse> GetAllTemplatesAsync();
+        public Task<GetTemplatesResponse> GetTemplateByIdAsync(int id); 
+
+    }
+
+}
---
+++ TemplateManager.Application/Services/v1/TemplateConfigService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemplateManager.Application.Contracts.v1.Templates.Response;
+
+namespace TemplateManager.Application.Services.v1
+{
+    public class TemplateConfigService : ITemplateConfigService
+    {
+        private readonly HttpClient _httpClient;
+        public TemplateConfigService(HttpClient httpClient) 
+        {
+            _httpClient = httpClient;
+        }
+
+
+        #region --- (Functions not implemented by the TemplateManager API) ---
+        public Task<GetTemplatesResponse> GetAllTemplatesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetTemplatesResponse> GetTemplateByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+} 
+
