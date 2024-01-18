a96f3384aabf61fb94458e7e1f307413bdeafdc3
joelant97 <joelant97@gmail.com>
2024-01-18 03:16:25 -0400

    Edit Template + Used Handlebars Compile

--- GenPDFGateway.WebApi/Program.cs
+++ GenPDFGateway.WebApi/Program.cs
@@ -1,6 +1,6 @@
+using HandlebarsDotNet;
 using IronPdf;
 using IronPdf.Rendering;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,30 +20,36 @@ app.MapPost("/generator", () =>
     //TODO:Logic to get the data from TempleManagerAPI
     //And Custom the info with the Client App info Context.
 
-    ChromePdfRenderer renderer = new ChromePdfRenderer();
-
-    var htmlTemplate = "<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<title>Carta de Solicitud</title>\r\n" +
-    "</head>\r\n<body>\r\n<h1>Carta de Solicitud</h1>\r\n<p>Estimado/a [[AddresseeName]],</p>\r\n" +
-    "<p>Me dirijo a usted para expresar mi interés en el puesto de [[DesiredPosition]]\r\nen su empresa. " +
-    "Soy un/a profesional con experiencia en el campo y estoy\r\nentusiasmado/a con la oportunidad de formar" +
-    " parte de su equipo.</p>\r\n<p>Adjunto a esta carta encontrará mi currículum vitae, que detalla mi " +
-    "formación\r\nacadémica y mi experiencia laboral relevante. Estoy seguro/a de que mis\r\nhabilidades y " +
-    "conocimientos en [[RelevantAreas]] serán un valioso aporte para su\r\norganización.</p>\r\n<p>Quedo a su " +
-    "disposición para proporcionar cualquier información adicional que\r\npueda necesitar. Agradezco de antemano " +
-    "su consideración y espero tener la\r\noportunidad de discutir cómo puedo contribuir al éxito de su empresa." +
-    "</p>\r\n<p>Atentamente,</p>\r\n<p>[[AuthorName]]</p>\r\n<p>[[CurrentDate]]</p>\r\n</body>\r\n</html>";
+    var htmlTemplate = "<!DOCTYPE html>\r\n\t<html>\r\n\t<head>\r\n\t<title>Carta de Solicitud</title>" +
+    "\r\n\t</head>\r\n\t<body>\r\n\t<h1>Carta de Solicitud</h1>\r\n\t<p>Estimado/a {{AddresseeName}},</p>" +
+    "\r\n\t<p>Me dirijo a usted para expresar mi interés en el puesto de {{DesiredPosition}}\r\n\ten su empresa." +
+    " Soy un/a profesional con experiencia en el campo y estoy\r\n\tentusiasmado/a con la oportunidad de formar " +
+    "parte de su equipo.</p>\r\n\t<p>Adjunto a esta carta encontrará mi currículum vitae, que detalla mi formación" +
+    "\r\n\tacadémica y mi experiencia laboral relevante. Estoy seguro/a de que mis\r\n\thabilidades y conocimientos" +
+    " en {{RelevantAreas}} serán un valioso aporte para su\r\n\torganización.</p>\r\n\t<p>Quedo a su disposición para" +
+    " proporcionar cualquier información adicional que\r\n\tpueda necesitar. Agradezco de antemano su consideración y" +
+    " espero tener la\r\n\toportunidad de discutir cómo puedo contribuir al éxito de su empresa.</p>\r\n\t" +
+    "<p>Atentamente,</p>\r\n\t<p>{{AuthorName}}</p>\r\n\t<p>{{CurrentDate}}</p>\r\n\t</body>\r\n\t</html>";
 
 
-    string addresseeName = "@Grupo Punta Cana";
-    string desiredPosition = "Desarrollador";
-    string relevantAreas = "Desarrollo de Software";
     string authorName = "Joel Antonio";
     string currentDate = DateTime.Now.Date.ToString("yyyy-MM-dd");
     string currentDate2 = DateTime.Now.Millisecond.ToString();
 
-    var htmlInstance = htmlTemplate.Replace("[[AddresseeName]]", addresseeName);
-    var pdf = renderer.RenderHtmlAsPdf(htmlInstance);
-    pdf.SaveAs( "Documento_"+authorName+"_"+currentDate+"-"+currentDate2+".pdf");
+    var template = Handlebars.Compile(htmlTemplate);
+
+    var data = new { 
+        AddresseeName = "Grupo Punta Cana",
+        DesiredPosition = "Desarrollador",
+        RelevantAreas = "Desarrollo de Software",
+        AuthorName = authorName,
+        CurrentDate = currentDate
+        };
+    var result = template(data);
+
+    var renderer = new ChromePdfRenderer();
+    var pdf = renderer.RenderHtmlAsPdf(result);
+    pdf.SaveAs( "Doc_"+authorName+"_"+currentDate+"-"+currentDate2+".pdf");
 
 });
 
